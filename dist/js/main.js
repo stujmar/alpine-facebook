e006951e463d236e683c4b4c490f9b310ce87144
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-10 20:48:54 -0500

    [sm] added the javascript to turn the menu on and off

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,17 +2,26 @@ console.log("loaded js");
 
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('nav');
+const navMenu = document.querySelector('nav-menu');
 
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+navMenu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        navMenu.classList.add('open');
         showMenu = true;
+
     } else {
         hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        navMenu.classList.remove('open');
         showMenu = false;
+
     }
 }
