fb4357eae07a6e5f1c5fdefd56428c5e4a2c1319
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-10 21:21:51 -0500

    [sm] got queryselector to work on the nested nav menu and it is all working nicely now

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,24 +3,27 @@ console.log("loaded js");
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn_burger');
 const nav = document.querySelector('nav');
-const navMenu = document.querySelector('nav-menu');
+const menu = document.querySelector('.nav-menu');
+console.log(menu);
 
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
-navMenu.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
+        console.log('opening');
         hamburger.classList.add('open');
         nav.classList.add('open');
-        navMenu.classList.add('open');
+        menu.classList.add('open');
         showMenu = true;
 
     } else {
+        console.log('close');
         hamburger.classList.remove('open');
         nav.classList.remove('open');
-        navMenu.classList.remove('open');
+        menu.classList.remove('open');
         showMenu = false;
 
     }
