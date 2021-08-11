6b65fe6311be753ffe7e4befe62803598e41e344
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-10 21:45:00 -0500

    [sm] styled and animated some of the classes on the menu items themselves

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,9 +2,8 @@ console.log("loaded js");
 
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn_burger');
-const nav = document.querySelector('nav');
+const nav = document.querySelector('.nav');
 const menu = document.querySelector('.nav-menu');
-console.log(menu);
 
 let showMenu = false;
 
@@ -13,14 +12,12 @@ menu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        console.log('opening');
         hamburger.classList.add('open');
         nav.classList.add('open');
         menu.classList.add('open');
         showMenu = true;
 
     } else {
-        console.log('close');
         hamburger.classList.remove('open');
         nav.classList.remove('open');
         menu.classList.remove('open');
