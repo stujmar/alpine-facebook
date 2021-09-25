91648c369568cc766b75f11e5dc10c104d62506b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-25 18:34:29 -0500

    [sm] got data to print and found some strange anomolies to trouble shoot

--- js/components/header.js
+++ js/components/header.js
@@ -7,7 +7,10 @@ fetch(fileUrl)
   .then(response => response.text())
   .then(data => {
     partial = data;
-    print(typeof data)
+    // print(typeof data)
+    console.log(typeof data)
+    console.log("`" + data + "`")
+    data = "`" + data + "`"
     headerPartial.innerHTML = partial;
     document.body.insertBefore(headerPartial.content, document.body.firstChild);
   });
