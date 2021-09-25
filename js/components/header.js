641913d4cdc8242a06f34b6b3e083d7735828178
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-25 18:28:44 -0500

    [sm] wow accidently printed a webpage trying to print  to the console

--- js/components/header.js
+++ js/components/header.js
@@ -1,12 +1,13 @@
-const headerPartial = document.createElement('template');
+const headerPartial = document.createElement("template");
 
-const fileUrl = './js/components/header.html';
-let partial = 'no data yet';
+const fileUrl = "./js/components/header.html";
+let partial = "no data yet";
 
 fetch(fileUrl)
   .then(response => response.text())
   .then(data => {
     partial = data;
+    print(typeof data)
     headerPartial.innerHTML = partial;
     document.body.insertBefore(headerPartial.content, document.body.firstChild);
   });
